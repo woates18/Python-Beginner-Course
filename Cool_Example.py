809274993c98ae37358cd300f04d5e834899606d
woates18 <133925518+woates18@users.noreply.github.com>
2024-02-21 14:40:31 -0500

    Hello World
    Simple Hello World Program

---
+++ Cool_Example.py
@@ -0,0 +1,11 @@
+"""
+Author: Will Oates
+Assignment: Cool Example
+Date: 1/20/2022
+Descritption: A cool example from my teacher
+
+"""
+# Output saying hello world 
+print("Hello")
+print("world")
+print("this is so much fun")
