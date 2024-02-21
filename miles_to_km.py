d60cc717138abfebbc210b622d10e244d76665c7
woates18 <133925518+woates18@users.noreply.github.com>
2024-02-21 14:41:32 -0500

    mile to kilometer
    Retrieves user input for miles and converts it to kilometers

---
+++ miles_to_km.py
@@ -0,0 +1,18 @@
+"""
+Author: William OAtes
+Class: CS021
+Date: 1/27/2022
+Description: Convert Miles to Km
+"""
+
+
+# [Input]
+# Get Values for miles
+Miles = int(input("Number of miles?: "))
+#Processing
+#Calculate km by the formula km = m * 1.60934
+Km = Miles * 1.60934
+
+#[Output]
+#Display calculated km
+print(Miles, " miles is equivalent to ", Km, " Kilometers")
